460f78c1a1945702e1bf6b083dec3cedd0dc6f80
tgorsk15 <tgbusiness463@gmail.com>
2023-12-03 19:29:49 -0600

    Add tabs to the navigation bar

--- src/index.js
+++ src/index.js
@@ -17,11 +17,24 @@ const siteNavigator = (function () {
 
     // create the main website bar at the top
     const navigationBar = document.createElement('div');
-    navigationBar.textContent = 'Here I am';
+    navigationBar.textContent = 'Bertie Bott\'s';
     navigationBar.classList.add('navigation-bar');
 
+    // adds the navigation bar to the top
     document.body.appendChild(navigationBar);
     body.insertBefore(navigationBar, pageHolder);
+
+
+    // array containing the tab names
+    const tabNames = ['Home', 'About', 'Menu', 'Reservations']
+
+    for (let i = 0; i < 4; i++) {
+        const tab = document.createElement('div');
+        tab.classList.add('navigation-tab');
+        tab.textContent = tabNames[i];
+        navigationBar.appendChild(tab);
+    }
+
 
 })();
 
@@ -33,5 +46,6 @@ const siteNavigator = (function () {
 // add addEventListeners to tab links at the top of page, so that when they
 //are clicked, the correct page is loaded in through the "content" div.
 
-// 
+// when one page is selected, the others should be set to
+// 'display: none'
 
