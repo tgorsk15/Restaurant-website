f20e4f5396c8e4801029fa64f8ee56284886e97b
tgorsk15 <tgbusiness463@gmail.com>
2023-12-04 15:26:49 -0600

    Build out home1 and provide structure for home1, 2, 3
    Inserted the first image/icon that the website will be using into home1. An 'imgs' folder was
    created in the src folder to contain all images the site uses going forward. On top of this,
    in the home.js file, each div will have it's own function that will be called on to add all the
    necessary elements and styling

--- src/home.js
+++ src/home.js
@@ -6,6 +6,8 @@
 //  - will have a small block of text wiht a short description
 
 // Div 3 will contain an img, something Wizarding-world-esque
+import frog from './imgs/TheFrogChoirHM.png'
+
 
 const homeContainer = document.createElement('div');
 
@@ -16,6 +18,7 @@ export const homePage = function () {
 
     // const body = document.body;
     const homeDivNames = ['home1', 'home2', 'home3'];
+    let homeDivs = [];
     
     // this won't be appended to the the "content" div until it is
     // called upon in the index.js file
@@ -27,12 +30,39 @@ export const homePage = function () {
     for (let i = 0; i < 3; i++) {
         const homeDiv = document.createElement('div');
         homeDiv.classList.add(homeDivNames[i]);
-        homeContainer.appendChild(homeDiv);      
-    }
+        homeContainer.appendChild(homeDiv);
+        homeDivs.push(homeDiv);
+    };
+    let home1 = homeDivs[0];
+    let home2 = homeDivs[1];
+    let home3 = homeDivs[2];
+
+
+    // style each homeDiv accordingly
+    // home1:
+    styleHome1(home1);
+    styleHome2(home2);
+    styleHome3(home3);
+
 
     console.log('homepage is linked');
 
     // return homeContainer
+
+}
+
+function styleHome1(home1) {
+    const frogIcon = new Image();
+    frogIcon.src = frog;
+    frogIcon.classList.add('frog-icon')
+    home1.appendChild(frogIcon);
+}
+
+function styleHome2 (home2) {
+
+}
+
+function styleHome3 (home3) {
 
 }
 
