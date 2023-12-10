541949fe6c152cfb70b909582787caf8193b9977
tgorsk15 <tgbusiness463@gmail.com>
2023-12-10 16:34:31 -0600

    Add structure and initial styling to Menu page

--- src/menu.js
+++ src/menu.js
@@ -9,6 +9,28 @@ const menuContainer = document.createElement('div');
 export const menuPage = function () {
     menuContainer.classList.add('menu-container');
     console.log('menu page linked');
+
+    //create 2main containers for the page
+    const menu1 = document.createElement('div')
+    menuFactory.addToMenu(menu1, 'menu1', menuContainer);
+
+    const menu2 = document.createElement('div')
+    menuFactory.addToMenu(menu2, 'menu2', menuContainer);
+
 };
+
+
+const menuFactory = (function () {
+
+    function addToMenu(elementName, className, fatherElement ) {
+        elementName.classList.add(className);
+        fatherElement.appendChild(elementName);
+
+        console.log(elementName);
+        return elementName;
+    };
+    return {addToMenu};
+})();
+
 
 export {menuContainer};
